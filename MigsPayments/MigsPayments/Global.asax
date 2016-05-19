292f3e62811c75b3b030f16eefb589d595636e44
A <contact@adamkirkbride.com>
2016-05-19 17:43:55 +1000

    First commit

---
+++ MigsPayments/MigsPayments/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MigsPayments.MvcApplication" Language="C#" %>
---
+++ MigsPayments/MigsPayments/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MigsPayments.Startup))]
+namespace MigsPayments
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
